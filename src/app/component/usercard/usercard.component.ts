d6c87146f9ef7273c87f8c74b96f08451ad202b9
alexktp <alexandre.malachin@gmail.com>
2022-06-05 09:43:58 +0200

    User Card v2 UI

--- src/app/component/usercard/usercard.component.ts
+++ src/app/component/usercard/usercard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-usercard',
   templateUrl: './usercard.component.html',
-  styleUrls: ['./usercard.component.css']
+  styleUrls: ['./usercard.component.scss']
 })
 export class UsercardComponent implements OnInit {
 
