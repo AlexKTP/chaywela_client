04aa8f0dbbb7718538124743df4d045956ca78f3
alexktp <alexandre.malachin@gmail.com>
2022-06-06 17:10:26 +0200

    Carousel & sidebar  Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule} from '@angular/common/http';
-import { UsercardComponent } from './component/usercard/usercard.component'
+import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { CarouselComponent } from './component/carousel/carousel.component';
+import { UserCardComponent } from './component/user-card/user-card.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsercardComponent
+    SidebarComponent,
+    CarouselComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
